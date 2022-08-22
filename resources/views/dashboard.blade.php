a78a819ed4cac01453de0a16ab139e1673a5b1b3
Rihards Ratke <rihards.ratke@magebit.com>
2022-08-22 13:51:33 +0300

    Updated Admin grid

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,24 +9,30 @@
 
 </head>
 <body class="antialiased">
+<div class="action">
+    <button>Add</button>
+</div>
 <table>
     <tr>
+        <th>Action</th>
         <th>Id</th>
         <th>Name</th>
         <th>Img</th>
         <th>Price</th>
+        <th>Action</th>
     </tr>
     @foreach($products as $product)
         <tr>
+            <td>
+                <input type="checkbox">
+            </td>
             <td>{{$product->id}}</td>
             <td>{{$product->name}}</td>
             <td>{{$product->img}}</td>
             <td>{{$product->price}}</td>
+            <td>Remove | Edit</td>
         </tr>
     @endforeach
-    <tr>
-
-    </tr>
 </table>
 </body>
 </html>
