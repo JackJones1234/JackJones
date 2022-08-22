289e4c8bd5edcfb3295135d5aa5ba40a3cc3b14f
N3mils <emilskrumins11@gmail.com>
2022-08-22 13:39:05 +0300

    title component

---
+++ resources/views/common/title.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1>Obsessive Attention. Intelligent Effort.</h1>
+    <h6>Functional handbags made of luxurious and honest materials to improve people's lives in small but mighty ways.</h6>
+</div>
