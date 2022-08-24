3123ece18886dad1f3e1331e8fffb1e7407b42a2
Edgars <ep98@inbox.lv>
2022-08-24 12:37:36 +0300

    3d rendering for products animation and styling improvements

--- public/js/home/app.js
+++ public/js/home/app.js
@@ -10,25 +10,19 @@ const products = document.querySelectorAll(".product-card");
 
 window.addEventListener("load", showOnScroll);
 window.addEventListener("scroll", showOnScroll);
+window.addEventListener("resize", showOnScroll);
+
 function showOnScroll() {
-    console.log('īm here');
     let screenPos = window.innerHeight;
     for (let i = 0; i < products.length; i++) {
         let imgPos = products[i].getBoundingClientRect().top;
         if (imgPos < screenPos) {
-            console.log('ādded appear')
             products[i].classList.add("appear");
         } else {
             products[i].classList.remove("appear"); // optional
         }
     }
 }
-
-// tilt effect
-products.forEach((e) => {
-    e.setAttribute("data-tilt", "");
-    e.setAttribute("data-tilt-speed", "5000");
-});
 
 // ***************************************************************
 
