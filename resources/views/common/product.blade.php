835ce4614b95da988f0d8bc494398291df732fb3
Edgars <ep98@inbox.lv>
2022-08-24 18:27:28 +0300

    product card design and animation improvement

--- resources/views/common/product.blade.php
+++ resources/views/common/product.blade.php
@@ -1,10 +1,12 @@
 <div class="product-container flex-center">
     @foreach($products as $product)
-        <div class="product-card" onclick="openProduct(this)" data-id="{{ $product->id }}" data-tilt data-tilt-speed="5000">
+        <div class="product-card" onclick="openProduct(this)" data-id="{{ $product->id }}" data-tilt data-tilt-max="20" data-tilt-speed="5000">
+            {{-- <img src="{{ asset('images/product/'. $product->img) }}" alt="product"> --}}
+            <img src="{{ asset('images/product/product.png') }}" alt="product">
+            
             <div class="product-info">
-                <div>{{ $product->name }}</div>
-                <div>${{ $product->price }}</div>
-                {{-- <img src="{{ asset('images/product/'. $product->img) }}" alt=""> --}}
+                <div class="product-name">{{ $product->name }}</div>
+                <div class="product-price">${{ $product->price }}</div>
             </div>
         </div>
     @endforeach
