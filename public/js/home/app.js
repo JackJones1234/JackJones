264d472680155413fc48f7a2679c894230499f39
Edgars <ep98@inbox.lv>
2022-08-24 09:15:01 +0300

    added appear on scroll animation for products

--- public/js/home/app.js
+++ public/js/home/app.js
@@ -1,4 +1,23 @@
 function openProduct(event) {
     const id = event.dataset.id;
-    location.replace('http://127.0.0.1:8000/product/' + id);
+    location.replace("http://127.0.0.1:8000/product/" + id);
 }
+
+// appear on scroll **********************************************
+const products = document.querySelectorAll(".product-card");
+
+window.addEventListener("load", showOnScroll);
+window.addEventListener("scroll", showOnScroll);
+function showOnScroll() {
+    let screenPos = window.innerHeight;
+    for (let i = 0; i < products.length; i++) {
+        let imgPos = products[i].getBoundingClientRect().top;
+        if (imgPos < screenPos) {
+            products[i].classList.add("appear");
+        } else {
+            products[i].classList.remove("appear"); // optional
+        }
+    }
+}
+
+// ***************************************************************
