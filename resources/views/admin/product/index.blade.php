e311e0c3472d15cd7c4baa72f6408fe542896a12
rkokins <M!6artis>
2022-08-25 21:25:11 +0300

    admin product view a bit better

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -5,27 +5,32 @@
     {{ Session::get('message') }}
 @endif
 <button onclick="location.href='/admin/product/add'">Add</button>
-<table>
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Price</th>
-      <th>Action</th>
-    </tr>
-    @foreach($products as $product)
+<div class="table-wrapper">
+    <table>
         <tr>
-        <td>{{ $product->id }}</td>
-        <td>{{ $product->name }}</td>
-        <td>{{ $product->price }}</td>
-        <td>
-          <form action="/admin/product/delete" method="POST">
-            @csrf
-            <input type="submit" value="Delete">
-            <input type="hidden" name='product_id' value={{ $product->id }}>
-          </form>
-          <button onclick="location.href='/admin/product/edit/{{ $product->id }}'">Edit</button>
-        </td>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Action</th>
         </tr>
-    @endforeach
-  </table>
+        @foreach($products as $product)
+            <tr>
+                <td>{{ $product->id }}</td>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->price }}</td>
+                <td>
+                    <div class="form-wrapper">
+                        <form action="/admin/product/delete" method="POST">
+                            @csrf
+                            <input type="submit" value="Delete">
+                            <input type="hidden" name='product_id' value={{ $product->id }}>
+                        </form>
+                        <button onclick="location.href='/admin/product/edit/{{ $product->id }}'">Edit</button>
+                    </div>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+</div>
+
 @endsection
