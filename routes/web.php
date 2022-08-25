f3fc0b21211b0979afa6f181f3c5f8d3c93988df
rkokins <M!6artis>
2022-08-25 20:45:13 +0300

    added route and controller for checkout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CheckoutController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminControllers\ProductController;
@@ -41,3 +42,4 @@ Route::get('/admin/product/edit/{id}', [ProductController::class, 'edit']);
 Route::post('/admin/product/delete', [ProductController::class, 'delete']);
 Route::post('/admin/product/edit', [ProductController::class, 'storeEdit']);
 
+Route::get('/checkout', [CheckoutController::class, 'index']);
