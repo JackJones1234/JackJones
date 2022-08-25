76396c27d2f2c95ac1ab48b9be075d0526af35a9
Askolds Darnopihs <askolds.darnopihs@gmail.com>
2022-08-25 11:16:17 +0300

    working on cart

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -1,20 +1,22 @@
 @extends('layout.master')
 
 @section('content')
-    <a href="{{ url('') }}">Go to Storefront</a>
-    @if(gettype($products) == "array")
-        @foreach($products as $product)
-            <div>
-                <div>{{ $product['product']->name }}</div>
-                <div>{{ $product['quantity'] }}</div>
-                <div>{{ $product['product']->price }}</div>
-            </div>
-        @endforeach
-    @else
-       <div>
-           <div>{{ $products->name }}</div>
-           <div>{{ $products['quantity'] }}</div>
-           <div>{{ $products->price }}</div>
-       </div>
-    @endif
+    <div class="main-container">
+        <a class="cart-storefront-btn" href="{{ url('') }}">Go to Storefront</a>
+        @if(gettype($products) == "array")
+            @foreach($products as $product)
+                <div class="cart-product-container">
+                    <div>{{ $product['product']->name }}</div>
+                    <div>{{ $product['quantity'] }}</div>
+                    <div>{{ $product['product']->price }}</div>
+                </div>
+            @endforeach
+        @else
+        <div>
+            <div>{{ $products->name }}</div>
+            <div>{{ $products['quantity'] }}</div>
+            <div>{{ $products->price }}</div>
+        </div>
+        @endif
+    </div>
 @endsection
