f2a223a85f44e405d72ea79e8002213d852420f0
Edgars <ep98@inbox.lv>
2022-08-25 11:14:30 +0300

    fixed styles dir structure

--- resources/views/common/product.blade.php
+++ resources/views/common/product.blade.php
@@ -1,3 +1,4 @@
+{{-- all products --}}
 <div class="product-container">
     @foreach($products as $product)
         <div class="product-card" onclick="openProduct(this)" data-id="{{ $product->id }}" data-tilt data-tilt-max="20" data-tilt-speed="5000">
