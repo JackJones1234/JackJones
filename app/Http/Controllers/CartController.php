f1a3465c82382f9e30db533785e58986d3923c7f
“rkokins” <kkokinss.robis@gmail.com>
2022-08-25 10:52:47 +0300

    fixed cookie for cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -16,28 +16,20 @@ class CartController extends Controller
             ->first();
         $model->cart_id = $_COOKIE['cart'];
 
-        if(!isset($data)) { //ja nav datu baze ieraksts ar tadu cookie un prod_id tad to ieliek card tabula ar izveleto produktu
+        if(!isset($data)) { 
             $model->product_id = $request->input('product_id');
             $model->quantity = $request->input('product_quantity');
             $model->save();
-            $product = Product::where('id', $request->input('product_id'))->first();
-            return view('pages.cart', ['products' => $product, 'quantity' => $model->quantity]);
-        }
-
-        //ja ir jau tads ieraksts tad to vajag update tikai pec quantity
-
-        // $model->product_id = $data->product_id . ',' . $request->input('product_id');
-        $model
+            $product[] = ['product' => Product::where('id', $request->input('product_id'))->first(), 'quantity' => $request->input('product_quantity')];
+        } else {
+            $model
             ->where('cart_id', $_COOKIE['cart'])
             ->where('product_id', $request->input('product_id'))
             ->update([
                 'quantity' => $request->input('product_quantity')
             ]);
-        
+        }
 
-        // foreach(explode(',', $model->product_id) as $id) {
-        //     $products[] = Product::where('id', $id)->first();
-        // }
         $products = $model->where('cart_id', $_COOKIE['cart'])->get();
         $ids = [];
         foreach($products as $product) {
--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -5,16 +5,16 @@
     @if(gettype($products) == "array")
         @foreach($products as $product)
             <div>
-                <div>{{ $product['product']->name }}</div>
-                <div>{{ $product['quantity'] }}</div>
-                <div>{{ $product['product']->price }}</div>
+                <div>Name: {{ $product['product']->name }}</div>
+                <div>Quantity: {{ $product['quantity'] }}</div>
+                <div>Price for 1: {{ $product['product']->price }}</div>
             </div>
         @endforeach
     @else
        <div>
-           <div>{{ $products->name }}</div>
-           <div>{{ $products['quantity'] }}</div>
-           <div>{{ $products->price }}</div>
+           <div>Namee: {{ $products->name }}</div>
+           <div>Quantityy: {{ $products['quantity'] }}</div>
+           <div>Price for 1ss: {{ $products->price }}</div>
        </div>
     @endif
 @endsection
