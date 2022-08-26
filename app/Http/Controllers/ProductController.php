6bf1622303c2b916d6db0a38b96b485d79f9b5c4
“rkokins” <kkokinss.robis@gmail.com>
2022-08-26 12:31:56 +0300

    added search funcionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use PHPUnit\Runner\Hook;
 
 class ProductController extends Controller
 {
@@ -11,4 +13,16 @@ class ProductController extends Controller
         $product = Product::where('id', $id)->first();
         return view('pages.product', ['product' => $product]);
     }
+
+    public function search(Request $request) {
+        $product = new Product();
+        $products = DB::table('products')->where('name', 'like', "%". $request->input('search')."%")->get();
+        if(sizeof($products) == 0) {
+            // return view('pages.home', ['products' => Product::all(), 'search' => false]);
+        }
+        return view('pages.home', ['products' => $products]);
+    }
 }
+
+// name
+// name...."%nasme%"
--- resources/views/common/product.blade.php
+++ resources/views/common/product.blade.php
@@ -1,12 +1,17 @@
 {{-- all products --}}
 <div class="product-container">
-    @foreach($products as $product)
-        <div class="product-card" onclick="openProduct(this)" data-id="{{ $product->id }}" data-tilt data-tilt-max="20" data-tilt-speed="5000">
-             <img src="{{ asset('images/product/tablelamps/'. $product->img) }}" alt="product">
-              <div class="product-info">
-                <div>{{ $product->name }}</div>
-                <div>${{ $product->price }}</div>
+    @if(!sizeof($products))
+        <h2>Product not found</h2>
+        <a href="/">To homepage</a>
+    @else
+        @foreach($products as $product)
+            <div class="product-card" onclick="openProduct(this)" data-id="{{ $product->id }}" data-tilt data-tilt-max="20" data-tilt-speed="5000">
+                <img src="{{ asset('images/product/tablelamps/'. $product->img) }}" alt="product">
+                <div class="product-info">
+                    <div>{{ $product->name }}</div>
+                    <div>${{ $product->price }}</div>
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    @endif
 </div>
