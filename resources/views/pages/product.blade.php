200fd7d4e4e50dfeac42ba0b02ad02330f3d1586
Edgars <ep98@inbox.lv>
2022-08-26 10:24:29 +0300

    refactor of product page and responsive

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -1,30 +1,30 @@
 @extends('layout.master')
 
-
 @section('content')
-    <div class="product-wrapper">
+
         <div class="single-product-container">
-            <div class="product-card">
-                <img src="{{ asset('images/product/tablelamps/'. $product->img) }}" alt="product">
+            <img src="{{ asset('images/product/tablelamps/'. $product->img) }}" alt="product">
+
+            <div class="product-info">
+                <div class="product-name">{{ $product->name }}</div>
+                <div class="product-price">${{ $product->price }}</div>
+
+                <form action="/cart/add" method="POST">
+                    @csrf
+                    <div class="quantity">
+                        <input id="quantity-remove" type="button" onclick='removeQuantity(this.value);' value="-">
+                        <input id="quantity" type="text" name="product_quantity" value="1" readonly>
+                        <input id="quantity-add" type="button" onclick='addQuantity(this.value);' value="+">
+                    </div>
+    
+                    <input id="button-submit" type="submit" value="Add to cart">
+                    <input id="button-buyitnow" type="submit" value="But it now">
+                    <input type="hidden" name="product_id" value="{{ $product->id }}">
+                </form>
             </div>
-        </div>
-        <div class="product-info">
-            <div class="product-name">{{ $product->name }}</div>
-            <div class="product-price">${{ $product->price }}</div>
-            <form action="/cart/add" method="POST">
-                @csrf
-                <div class="quantity">
-                    <input id="quantity-remove" type="button" onclick='removeQuantity(this.value);' value="-">
-                    <input id="quantity" type="text" name="product_quantity" value="1" readonly>
-                    <input id="quantity-add" type="button" onclick='addQuantity(this.value);' value="+">
-                </div>
-                <br>
-                <input id="button-submit" type="submit" value="Add to cart">
-                <input id="button-buyitnow" type="submit" value="But it now">
-                <input type="hidden" name="product_id" value="{{ $product->id }}">
-            </form>
-        </div>
-    </div>
+        </div> 
+
     <script src="{{ asset('js/product/productQuantity.js')}}"></script>
+
 @endsection
 
